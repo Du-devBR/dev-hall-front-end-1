8746c3d3c911de05e8550b1cf1c14501e90b16f4
Du-devBR <89052479+Du-devBR@users.noreply.github.com>
2021-12-14 21:56:33 -0300

    add mask ao input phone number, revisado codigo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-const buttonMobile = document.getElementById('menu_mobile');
+// Menu telas menos que 768px
 
+
+const buttonMobile = document.getElementById('menu_mobile');
 
 function menuToggle(event){
   if (event.type === 'touchstart') event.preventDefault();
@@ -19,7 +21,8 @@ menu_mobile.addEventListener('click', menuToggle);
 menu_mobile.addEventListener('touchstart', menuToggle);
 
 //efeito nas letras
-
+//Função para manipular o texto em formato de arrays e tratar com split,
+// utilizando o metodo FOREACH incrementando letra por letra.
 const bannerTitle = document.querySelector('.title')
 
 function writingForm(element){
@@ -36,9 +39,17 @@ writingForm(bannerTitle)
 
 
 
+//
+
+
+
+
+
 
 
 // Validação formulario
+// Função para validar os campos de contato, sé será possivel enviar os dados preenchendo os campos de input.
+// Metodo criado em cima dos required obrigatorio nos campos.
 
 const requestFields = document.querySelectorAll('[required')
 
@@ -52,24 +63,45 @@ function customValidation(event){
      if(error != 'customError' && field.validity[error]){
        foundError = true
      }
-
     }
 
     return foundError;
-
-}
+  }
 const error = errors()
 console.log('erro', error)
 
-if(error){
-  field.setCustomValidity('Campo obrigatorio para contato')
-}
-else{
-  field.setCustomValidity('')
-}
+      if(error){
+        field.setCustomValidity('Campo obrigatorio para contato')
+      }
+      else{
+        field.setCustomValidity('')
+      }
 
 }
 
 for(const field of requestFields){
   field.addEventListener('invalid', customValidation)
+}
+
+
+
+//Utilizado mask para o campo de numero whatsapp.
+// Utilizado REGEX para o metodo de Replace.
+//Utilizado a função a baixo apenas para fins didaticos com replace e entender a fromatação de um campo digitado.
+// Utilizado a dica do site https://elcio.com.br/ajax/mascara/
+
+
+function maskPhoneNumber(o,f){
+  v_obj=o
+  v_fun=f
+  setTimeout("execmask()",0)
+}
+function execmask(){
+  v_obj.value=v_fun(v_obj.value)
+}
+function maskTel(v){
+  v=v.replace(/\D/g,"");             //Remove tudo o que não é dígito
+  v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+  v=v.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
+  return v;
 }
